a2a4d3e2c2d07e4c1a5943873e623dde01d883e0
Zakaria Zahi <zakaria.zahi@arm.com>
2021-09-16 13:14:13 +0530

    sgi/sct.sh: support UEFI SCT tests on additional platforms
    Allow UEFI SCT tests to be executed on additional platforms. This allows
    executing the UEFI SCT tests as an independent test outside of ACS test
    suite. Independent tests allows for selecting the tests to be executed.
    
    Signed-off-by: Zakaria Zahi <zakaria.zahi@arm.com>
    Change-Id: Ib004c2c4cfe7844891a453eba37b3806ec41f83f

--- sgi/sct.sh
+++ sgi/sct.sh
@@ -38,6 +38,11 @@ platforms_sgi[sgi575]=1
 declare -A platforms_rdinfra
 platforms_rdinfra[rdn1edge]=1
 platforms_rdinfra[rde1edge]=1
+platforms_rdinfra[rdv1]=1
+platforms_rdinfra[rdv1mc]=1
+platforms_rdinfra[rdn2]=1
+platforms_rdinfra[rdn2cfg1]=1
+platforms_rdinfra[rdn2cfg2]=1
 
 __print_examples()
 {
