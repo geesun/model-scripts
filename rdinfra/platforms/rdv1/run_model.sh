6d01cc2141c1538cf42d172b4ca3c3136fa9f84c
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2021-10-26 11:52:04 +0530

    rdv1/run_model: print uart logs location and model launch cmd
    Refactor RD-V1's run_model.sh script to print the full path of all the
    UART logs and the full model launch command.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Change-Id: I66a22594a0715ae1e3cd7a819bc6094505609d13

--- rdinfra/platforms/rdv1/run_model.sh
+++ rdinfra/platforms/rdv1/run_model.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2019, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2019-2021, ARM Limited and Contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -35,6 +35,7 @@ NORMAL_FONT="\e[0m"
 RED_FONT="\e[31;1m"
 GREEN_FONT="\e[32;1m"
 YELLOW_FONT="\e[33;1m"
+CYAN_FONT="\033[0;36m"
 ROOTDIR="../../../../output/rdv1"
 OUTDIR=${ROOTDIR}/rdv1
 MODEL_TYPE="rdv1"
@@ -221,13 +222,6 @@ if [ "$AUTOMATE" == "true" ] ; then
 		"
 fi
 
-echo
-echo "Starting model "$MODEL_TYPE
-echo "  MODEL_PARAMS = "$MODEL_PARAMS
-echo "  EXTRA_PARAMS = "$EXTRA_MODEL_PARAMS
-echo "  UART Log     = "$PWD/${MODEL_TYPE,,}/${UART0_ARMTF_OUTPUT_FILE_NAME}
-echo
-
 # print the model version.
 ${MODEL} --version
 
@@ -275,6 +269,17 @@ PARAMS="-C css.cmn_650.force_rnsam_internal=true \
 	${MODEL_PARAMS} \
 	${TZC_BYPASS_PARAMS} \
 	${EXTRA_MODEL_PARAMS}"
+
+echo
+echo "SCP UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_SCP_OUTPUT_FILE_NAME}
+echo "MCP UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_MCP_OUTPUT_FILE_NAME}
+echo "AP  UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_ARMTF_OUTPUT_FILE_NAME}
+echo "MM  UART Log = "$PWD/${MODEL_TYPE,,}/${UART1_MM_OUTPUT_FILE_NAME}
+echo
+echo -e "${GREEN_FONT}Launching RD-V1 model${NORMAL_FONT}"
+echo
+echo -e "${CYAN_FONT}${MODEL} ${PARAMS}${NORMAL_FONT}"
+echo
 
 if [ "$AUTOMATE" == "true" ] ; then
 	${MODEL} ${PARAMS} 2>&1 &
