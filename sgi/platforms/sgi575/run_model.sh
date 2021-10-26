420145431705c711385783027edcc69e1dd286df
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2021-10-26 11:36:38 +0530

    sgi575/run_model: print uart logs location and model launch cmd
    Refactor SGI-575's run_model.sh script to print the full path of all the
    UART logs and the full model launch command.
    
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
    Change-Id: I4f4cf48e378f6ec0ca32e2f31f670c345b3d9d3d

--- sgi/platforms/sgi575/run_model.sh
+++ sgi/platforms/sgi575/run_model.sh
@@ -15,6 +15,7 @@ NORMAL_FONT="\e[0m"
 RED_FONT="\e[31;1m"
 GREEN_FONT="\e[32;1m"
 YELLOW_FONT="\e[33;1m"
+CYAN_FONT="\033[0;36m"
 ROOTDIR="../../../../output/sgi575"
 OUTDIR=${ROOTDIR}/sgi575
 MODEL_TYPE="sgi575"
@@ -201,13 +202,6 @@ if [ "$AUTOMATE" == "true" ] ; then
 		"
 fi
 
-echo
-echo "Starting model "$MODEL_TYPE
-echo "  MODEL_PARAMS = "$MODEL_PARAMS
-echo "  EXTRA_PARAMS = "$EXTRA_MODEL_PARAMS
-echo "  UART Log     = "$PWD/${MODEL_TYPE,,}/${UART0_ARMTF_OUTPUT_FILE_NAME}
-echo
-
 # print the model version.
 ${MODEL} --version
 
@@ -240,6 +234,16 @@ PARAMS="-C css.cmn600.mesh_config_file=\"$PATH_TO_MODEL/SGI-575_cmn600.yml\" \
 	${MODEL_PARAMS} \
 	${EXTRA_MODEL_PARAMS}"
 
+echo
+echo "SCP UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_SCP_OUTPUT_FILE_NAME}
+echo "MCP UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_MCP_OUTPUT_FILE_NAME}
+echo "AP  UART Log = "$PWD/${MODEL_TYPE,,}/${UART0_ARMTF_OUTPUT_FILE_NAME}
+echo "MM  UART Log = "$PWD/${MODEL_TYPE,,}/${UART1_MM_OUTPUT_FILE_NAME}
+echo
+echo -e "${GREEN_FONT}Launching SGI-575 model${NORMAL_FONT}"
+echo
+echo -e "${CYAN_FONT}${MODEL} ${PARAMS}${NORMAL_FONT}"
+echo
 
 if [ "$AUTOMATE" == "true" ] ; then
 	${MODEL} ${PARAMS} 2>&1 &
